9a9e0799a18422dd198eff78ec2292163c4082aa
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-06-07 01:36:20 +0900

    fix submodule

--- run-tests.sh
+++ run-tests.sh
@@ -82,9 +82,8 @@ do
         --instance-ids "${SOURCE_INSTANCE_ID}" \
         --document-name "AWS-RunShellScript" \
         --comment "aws-ping command to run ping to all relevant EC2 instances in all the regions" \
-        --parameters commands=["/home/ec2-user/aws-ping-cross-region/ping-target.sh --source-region ${SOURCE_REGION} --target-region ${TARGET_REGION} --target-ip ${TARGET_IP_ADDRESS} --test-uuid ${TEST_EXECUTION_UUID}" --s3-bucket "${S3_BUCKET_NAME}"] \
-        --output text \
-        --query "Command.CommandId" > /dev/null ; then
+        --parameters commands=["/home/ec2-user/aws-ping-cross-region/ping-target.sh --source-region ${SOURCE_REGION} --target-region ${TARGET_REGION} --target-ip ${TARGET_IP_ADDRESS} --test-uuid ${TEST_EXECUTION_UUID} --s3-bucket ${S3_BUCKET_NAME}"] \
+        --region "${SOURCE_REGION}" > /dev/null ; then
         >&2 echo "ERROR: failed to send command to = ${SOURCE_INSTANCE_ID}"
       fi
 
