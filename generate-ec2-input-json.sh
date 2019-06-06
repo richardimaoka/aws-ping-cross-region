67ef915968466c25fac46c416c71c5734d2c860f
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-06-06 14:34:43 +0900

    fix

--- generate-ec2-input-json.sh
+++ generate-ec2-input-json.sh
@@ -2,8 +2,10 @@
 
 STACK_NAME="PingCrossRegionExperiment"
 
+FILE_NAME=$(tmpfile)
+
 # Start of JSON
-echo "{"
+echo "{" >> "${FILE_NAME}"
 
 LAST_REGION=$(aws ec2 describe-regions --query "Regions[].[RegionName]" --output text | tail -1)
 for REGION in $(aws ec2 describe-regions --query "Regions[].[RegionName]" --output text)
@@ -21,16 +23,20 @@ do
   SECURITY_GROUP_ID=$(echo "${OUTPUTS}" | jq -r '.[] | select(.OutputKey=="SecurityGroup") | .OutputValue')
   SUBNET_ID=$(echo "${OUTPUTS}" | jq -r '.[] | select(.OutputKey=="Subnet") | .OutputValue')
 
-  echo "\"${REGION}\": {"
-  echo "  \"image_id\": \"${AMI_LINUX2}\","
-  echo "  \"security_group\": \"${SECURITY_GROUP_ID}\","
-  echo "  \"subnet_id\": \"${SUBNET_ID}\""
-  if [ "$REGION" = "${LAST_REGION}" ]; then
-    echo "}"
+  echo "\"${REGION}\": {" >> "${FILE_NAME}"
+  echo "  \"image_id\": \"${AMI_LINUX2}\"," >> "${FILE_NAME}"
+  echo "  \"security_group\": \"${SECURITY_GROUP_ID}\"," >> "${FILE_NAME}"
+  echo "  \"subnet_id\": \"${SUBNET_ID}\"" >> "${FILE_NAME}"
+  if [ "$REGION" = "${LAST_REGION}" ]; then 
+    echo "}" >> "${FILE_NAME}"
   else
-    echo "},"
+    echo "}," >> "${FILE_NAME}"
   fi
 done
 
 # End of JSON
-echo "}"
+echo "}" >> "${FILE_NAME}"
+
+jq -s '.[0] * .[1]' "${FILE_NAME}" instance-types.json
+
+
